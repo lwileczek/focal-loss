aaad31875987a6ff8062f3d3a23310ba02da1e4f
lwileczek-dmc <lukewileczek@datamachines.io>
2022-01-25 20:18:24 -0500

    remove unused code from school

--- losses/focal_loss.py
+++ losses/focal_loss.py
@@ -4,25 +4,21 @@ import torch.nn.functional as F
 import numpy as np
 
 def reweight(cls_num_list, beta=0.9999):
-    '''
+    """
     Implement reweighting by effective numbers
     :param cls_num_list: a list containing # of samples of each class
     :param beta: hyper-parameter for reweighting, see paper for more details
     :return:
-    '''
+    """
     per_cls_weights = None
-    #############################################################################
-    #       reweight each class by effective numbers                            #
-    #############################################################################
+
     c = len(cls_num_list)
     per_cls_weights = np.zeros(c)
     for i,n in enumerate(cls_num_list):
         per_cls_weights[i] =  (1 - beta) / (1 - beta**n)  # a is inverse of En
     # Normalize so sum_1^C \alpha = C
     per_cls_weights *= (c / np.sum(per_cls_weights))
-    #############################################################################
-    #                              END OF YOUR CODE                             #
-    #############################################################################
+
     return torch.from_numpy(per_cls_weights)
 
 
@@ -34,16 +30,14 @@ class FocalLoss(nn.Module):
         self.weight = weight
 
     def forward(self, input, target):
-        '''
+        """
         Implement forward of focal loss
         :param input: input predictions
         :param target: labels
         :return: tensor of focal loss in scalar
-        '''
+        """
         loss = None
-        #############################################################################
-        #       Implement forward pass of the focal loss                            #
-        #############################################################################
+
         """
         The original Focal Loss Paper provides code for how to use Focal Loss:
           - https://www.paperswithcode.com/method/focal-loss
@@ -53,15 +47,9 @@ class FocalLoss(nn.Module):
         comments to show my understanding of the code.
         """
         N, _ = input.shape
-        # print("Shape of inputs:", input.shape)
-        # print("Shape of targets:", target.shape)
-        log_probs = F.log_softmax(input)    # Take the softmax and log of model outputs
-        # https://pytorch.org/docs/stable/generated/torch.gather.html
-        log_probs = log_probs[np.arange(N), target]  # Select from our labels the our models prediction of the true value
-        probs = log_probs.data.exp()  # Exponentiate to get the probababilities for each class
-        # Focal Loss with the weights being pulled from our class-balanced formula
+        log_probs = F.log_softmax(input)
+        log_probs = log_probs[np.arange(N), target]
+        probs = log_probs.data.exp()
         loss = -1 * self.weight[target]*(1-probs)**self.gamma * log_probs
-        #############################################################################
-        #                              END OF YOUR CODE                             #
-        #############################################################################
+
         return loss.sum()
